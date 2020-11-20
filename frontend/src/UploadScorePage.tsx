38e8c410538afe78820e45f0f473c907085c690d
uttne <uttne.mkr@gmail.com>
2020-11-21 00:06:28 +0900

    画像のアップロードのテストコードを追加

--- frontend/src/UploadScorePage.tsx
+++ frontend/src/UploadScorePage.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import GenericTemplate from './GenericTemplate'
+import { FormControl, FormHelperText, Input, InputLabel } from '@material-ui/core'
 
 const UploadScorePage = () => {
+  const [file, setFile] = React.useState('');
+
+  const handleUploadClick = (event: any) => {
+    console.log();
+    let file = event.target.files[0];
+    const reader = new FileReader();
+    let url = reader.readAsDataURL(file);
+    reader.onloadend = (e) =>{
+      setFile(reader.result as string);
+    }
+
+    console.log(url);
+    setFile(file);
+  }
+
   return (
     <GenericTemplate title="アップロードスコア">
-      <div></div>
+      <div>
+        <FormControl >
+          <InputLabel></InputLabel>
+          <Input aria-describedby="" type="file" inputProps={{ accept: "image/*"}} onChange={handleUploadClick}></Input>
+          <FormHelperText></FormHelperText>
+        </FormControl>
+        <img src={file}></img>
+      </div>
     </GenericTemplate>
   )
 }
