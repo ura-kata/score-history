a5963d86ab4dfd08732d76c638806436edacdb96
uttne <uttne.mkr@gmail.com>
2020-11-29 02:00:56 +0900

    API テストページの移動とメタデータ用のテストコンポーネントを追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {  BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import UploadScorePage from './components/pages/UploadScorePage';
 import DisplayPage from './components/pages/DisplayPage'
-import ApiTest from './ApiTest';
+import ApiTestPage from './components/pages/ApiTestPage';
 
 
 const App = () => {
@@ -11,7 +11,7 @@ const App = () => {
       <Switch>
         <Route path="/" component={UploadScorePage} exact />
         <Route path="/display" component={DisplayPage} exact />
-        <Route path="/api-test" component={ApiTest} exact />
+        <Route path="/api-test" component={ApiTestPage} exact />
       </Switch>
 
     </Router>
---
+++ frontend/src/components/pages/ApiTestPage.tsx
@@ -0,0 +1,171 @@
+import React, { useCallback, useState } from "react";
+import GenericTemplate from "../templates/GenericTemplate";
+import {
+  createStyles,
+  Grid,
+  Paper,
+  FormControl,
+  FormHelperText,
+  Input,
+  Button,
+  InputLabel,
+  makeStyles,
+  Theme,
+  colors,
+  Typography,
+  TextField,
+} from "@material-ui/core";
+import PracticeManagerApiClient, { NewScore, Socre } from "../../PracticeManagerApiClient";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    img: {
+      height: "50vh",
+    },
+    imageDropZoneRoot: {
+      margin: "20px",
+    },
+    paper: {
+      padding: theme.spacing(2),
+      margin: "auto",
+      width: "90%",
+    },
+  })
+);
+
+const apiClient = new PracticeManagerApiClient("http://localhost:5000/");
+
+const ApiTestPage = () => {
+  const classes = useStyles();
+  const [version, setVersion] = React.useState('');
+
+  // getVersion
+  const handlerGetVersion = useCallback(async () => {
+    try{
+      const version = await apiClient.getVersion();
+      setVersion(version);
+      alert("Success");
+    } catch(err) {
+      alert(err);
+    }
+
+  },[]);
+
+  const getVersionTag = (
+    <Grid item xs={12}>
+      <Paper className={classes.paper}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography>getVersion</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Button variant="outlined" color="primary" onClick={handlerGetVersion}>Exec</Button>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography>{version}</Typography>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Grid>
+  );
+
+
+  // createScore
+  const [createScore_param,setCreateScore_param] = useState<NewScore>({
+    name: "",
+    title: "",
+    description: "",
+  });
+  const handlerCreateScore = useCallback(async () => {
+    const newScore: NewScore = createScore_param;
+    try{
+      await apiClient.createScore(newScore);
+      alert("Success");
+    } catch(err){
+      alert(err);
+    }
+  }, [createScore_param]);
+
+  const handlerCreateScoreName = useCallback( (event) => {
+    setCreateScore_param({
+      name: event.target.id === 'createScore_param_name' ? event.target.value : createScore_param.name,
+      title: event.target.id === 'createScore_param_title' ? event.target.value : createScore_param.title,
+      description: event.target.id === 'createScore_param_description' ? event.target.value : createScore_param.description,
+    });
+  }, [createScore_param])
+
+  const createScoreTag = (
+    <Grid item xs={12}>
+      <Paper className={classes.paper}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography>createScore</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField id={'createScore_param_name'} value={createScore_param.name} onChange={handlerCreateScoreName} label={'name'}></TextField>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField id={'createScore_param_title'} value={createScore_param.title} onChange={handlerCreateScoreName} label={'title'}></TextField>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField id={'createScore_param_description'} value={createScore_param.description} onChange={handlerCreateScoreName} label={'description'}></TextField>
+          </Grid>
+          <Grid item xs={12}>
+            <Button variant="outlined" color="primary" onClick={handlerCreateScore}>Exec</Button>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Grid>
+  );
+
+
+  // getScores
+  const [getScores_scores,setGetScores_scores] = useState<Socre[]>([]);
+  const handlerGetScores = useCallback(async () => {
+    try{
+      const scores = await apiClient.getScores();
+      setGetScores_scores(scores);
+      alert("Success");
+    } catch(err){
+      alert(err);
+    }
+  }, []);
+
+  const getScoresTag = (
+    <Grid item xs={12}>
+      <Paper className={classes.paper}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography>getScores</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Button variant="outlined" color="primary" onClick={handlerGetScores}>Exec</Button>
+          </Grid>
+          {
+            getScores_scores.map((score, index)=>{
+              return (
+                <Grid item xs={12} key={index.toString()}>
+                  <Typography>{JSON.stringify(score)}</Typography>
+                </Grid>
+              );
+            })
+          }
+        </Grid>
+      </Paper>
+    </Grid>
+  );
+
+  return (
+    <GenericTemplate title="アップロードスコア">
+      <div>
+        <Grid container spacing={3}>
+          {getVersionTag}
+          {createScoreTag}
+          {getScoresTag}
+        </Grid>
+      </div>
+    </GenericTemplate>
+  );
+};
+
+export default ApiTestPage;
