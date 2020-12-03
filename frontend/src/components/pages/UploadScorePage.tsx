d58dfecc9ae552e6653ff1652832708ee6edd3c2
uttne <uttne.mkr@gmail.com>
2020-12-04 01:05:59 +0900

    アップロードしたスコア選択して表示する

--- frontend/src/components/pages/UploadScorePage.tsx
+++ frontend/src/components/pages/UploadScorePage.tsx
@@ -2,19 +2,16 @@ import React, {useCallback} from 'react';
 import GenericTemplate from '../templates/GenericTemplate'
 import { createStyles, FormControl, FormHelperText, Input, Button, InputLabel, makeStyles, Theme, colors, GridList, GridListTile, GridListTileBar, Grid, TextField, Dialog, DialogTitle, DialogContent, DialogActions } from '@material-ui/core'
 import { DialogProps } from '@material-ui/core/Dialog'
-import { DataGrid, ColDef, ValueGetterParams, Row } from '@material-ui/data-grid';
+import { DataGrid, ColDef, ValueGetterParams, Row, ValueFormatterParams } from '@material-ui/data-grid';
 import { useDropzone } from 'react-dropzone'
 import { readBuilderProgram } from 'typescript';
-import PracticeManagerApiClient, {Score} from '../../PracticeManagerApiClient'
+import PracticeManagerApiClient, {Score, SocreVersionMetaUrl} from '../../PracticeManagerApiClient'
 import { Alarm } from '@material-ui/icons';
+
+import ScoreViewr from '../atoms/ScoreViewer'
 
 const client = new PracticeManagerApiClient("http://localhost:5000/");
 
-const columns: ColDef[] = [
-  {field: 'name', headerName: '名前', width: 200},
-  {field: 'title', headerName: 'タイトル', width: 200},
-  {field: 'description', headerName: '説明', width: 500},
-]
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -51,8 +48,10 @@ interface FileData{
 
 interface Row{
   id: number;
+  open: {name: string, versionMetas: SocreVersionMetaUrl[]};
   name: string;
   title: string;
+  lastVersion: number;
   description: string;
 }
 
@@ -63,17 +62,17 @@ const UploadScorePage = () => {
   const [uploadScoreTitle, setUploadScoreTitle] = React.useState("");
   const [uploadScoreDescription, setUploadScoreDescription] = React.useState("");
 
-  const [dialogOpen, setDialogOpen] = React.useState(false);
-  const [dialogScroll, setDialogScroll] = React.useState<DialogProps['scroll']>('paper');
+  const [uploadDialogOpen, setDialogOpen] = React.useState(false);
+  const [uploadDialogScroll, setDialogScroll] = React.useState<DialogProps['scroll']>('paper');
 
   const [rows, setRows] = React.useState([] as Row[])
 
-  const handleDialogOpen = () => {
+  const handleUploadDialogOpen = () => {
     setDialogOpen(true);
     setDialogScroll('paper');
   };
 
-  const handleDialogClose = () => {
+  const handleUploadDialogClose = () => {
     setDialogOpen(false);
   }
 
@@ -114,7 +113,7 @@ const UploadScorePage = () => {
       });
       await client.createVersion(uploadScoreName, fileDataList.map(x=>x.file));
       alert('画像をアップロードしました');
-      handleDialogClose();
+      handleUploadDialogClose();
 
       setUploadScoreName("");
       setUploadScoreTitle("");
@@ -141,8 +140,13 @@ const UploadScorePage = () => {
 
       const r = scores.map((x,i)=>({
         id: i,
+        open: {
+          name: x.name,
+          versionMetas: x.version_meta_urls
+        },
         name: x.name,
         title: x.title,
+        lastVersion: x.version_meta_urls.slice(-1)[0].version,
         description: x.description
       }));
 
@@ -151,6 +155,126 @@ const UploadScorePage = () => {
       alert('更新に失敗しました');
     }
   },[]);
+
+
+  const uploadDialog = (
+    <Dialog
+      open={uploadDialogOpen}
+      onClose={handleUploadDialogClose}
+      scroll={uploadDialogScroll}
+    >
+      <DialogTitle>スコアをアップロードします</DialogTitle>
+      <DialogContent dividers={true}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <TextField value={uploadScoreName} onChange={handlerUploadScoreName} label={'スコアの名前'}></TextField>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField value={uploadScoreTitle} onChange={handlerUploadScoreTitle} label={'スコアのタイトル'}></TextField>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField value={uploadScoreDescription} onChange={handlerUploadScoreDescription} label={'スコアの説明'}></TextField>
+          </Grid>
+          <Grid item xs={12}>
+            <div className={classes.imageDropZoneRoot}>
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <div className={classes.imageDropZone}>
+                  このエリアをクリックするするか画像ドロップしてアップロードしてください
+                </div>
+              </div>
+            </div>
+          </Grid>
+          <Grid item xs={12}>
+            <GridList className={classes.imageList}>
+              {
+                fileDataList.map((fd, i) => (
+                  <GridListTile key={'image' + i.toString()}>
+                    <img src={fd.fileUrl} className={classes.img} alt={fd.file.name}></img>
+                    <GridListTileBar title={fd.file.name}/>
+                  </GridListTile>
+                ))
+              }
+            </GridList>
+          </Grid>
+        </Grid>
+
+      </DialogContent>
+      <DialogActions>
+        <Button variant="outlined" color="primary" onClick={handlerUpload}>アップロード</Button>
+        <Button variant="outlined" color="primary" onClick={handleUploadDialogClose}>キャンセル</Button>
+      </DialogActions>
+    </Dialog>
+  );
+
+
+  const [versionDialogOpen, setVersionDialogOpen] = React.useState(false);
+  const [versionDialogScroll, setVersionDialogScroll] = React.useState<DialogProps['scroll']>('paper');
+  const [imageUrls, setImageUrls] = React.useState([] as string[]);
+
+  const handleVersionDialogClose = useCallback(async ()=>{
+    setVersionDialogOpen(false);
+  },[]);
+
+  const columns: ColDef[] = [
+    {
+      field: 'open',
+      headerName: ' ',
+      renderCell: (params: ValueFormatterParams) => {
+        const item = (params.value as {name: string, versionMetas: SocreVersionMetaUrl[]});
+        console.log(item.name);
+        const handleVersionDialogOpenRowButton = async ()=>{
+          var versionMeta = item.versionMetas.slice(-1)[0];
+          const scoreVersion = await client.getScoreVersion(item.name, versionMeta.version);
+
+          setImageUrls(scoreVersion.pages.map(x=>x.url));
+
+          setVersionDialogOpen(true);
+
+        };
+
+        return (
+          <strong>
+            <Button
+              variant="contained"
+              color="primary"
+              size="small"
+              style={{margin: 8}}
+              onClick={handleVersionDialogOpenRowButton}
+            >Open</Button>
+          </strong>
+        )
+      }
+    },
+    {field: 'name', headerName: '名前', width: 200},
+    {field: 'title', headerName: 'タイトル', width: 200},
+    {field: 'lastVersion', headerName: '最新バージョン', width: 200},
+    {field: 'description', headerName: '説明', width: 500},
+  ];
+
+  const versionDialog = (
+    <Dialog
+      fullScreen
+      open={versionDialogOpen}
+      onClose={handleVersionDialogClose}
+      scroll={versionDialogScroll}
+    >
+      <DialogTitle>バージョン表示</DialogTitle>
+      <DialogContent dividers={true}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} style={{textAlign: "center"}}>
+            <div style={{width: "90vw", height: "80vh", display: "inline-block"}} >
+              <ScoreViewr imageUrls={imageUrls}></ScoreViewr>
+            </div>
+          </Grid>
+        </Grid>
+
+      </DialogContent>
+      <DialogActions>
+        <Button variant="outlined" color="primary" onClick={handleVersionDialogClose}>閉じる</Button>
+      </DialogActions>
+    </Dialog>
+  );
 
   return (
     <GenericTemplate title="スコアの一覧">
@@ -165,59 +289,14 @@ const UploadScorePage = () => {
             </div>
           </Grid>
           <Grid item xs={12}>
-            <Button variant="outlined" color="primary" onClick={handleDialogOpen}>アップロードする</Button>
+            <Button variant="outlined" color="primary" onClick={handleUploadDialogOpen}>アップロードする</Button>
           </Grid>
         </Grid>
 
-        <Dialog
-          open={dialogOpen}
-          onClose={handleDialogClose}
-          scroll={dialogScroll}
-        >
-          <DialogTitle>スコアをアップロードします</DialogTitle>
-          <DialogContent dividers={true}>
-            <Grid container spacing={3}>
-              <Grid item xs={12}>
-                <TextField value={uploadScoreName} onChange={handlerUploadScoreName} label={'スコアの名前'}></TextField>
-              </Grid>
-              <Grid item xs={12}>
-                <TextField value={uploadScoreTitle} onChange={handlerUploadScoreTitle} label={'スコアのタイトル'}></TextField>
-              </Grid>
-              <Grid item xs={12}>
-                <TextField value={uploadScoreDescription} onChange={handlerUploadScoreDescription} label={'スコアの説明'}></TextField>
-              </Grid>
-              <Grid item xs={12}>
-                <div className={classes.imageDropZoneRoot}>
-                  <div {...getRootProps()}>
-                    <input {...getInputProps()} />
-                    <div className={classes.imageDropZone}>
-                      このエリアをクリックするするか画像ドロップしてアップロードしてください
-                    </div>
-                  </div>
-                </div>
-              </Grid>
-              <Grid item xs={12}>
-                <GridList className={classes.imageList}>
-                  {
-                    fileDataList.map((fd, i) => (
-                      <GridListTile key={'image' + i.toString()}>
-                        <img src={fd.fileUrl} className={classes.img} alt={fd.file.name}></img>
-                        <GridListTileBar title={fd.file.name}/>
-                      </GridListTile>
-                    ))
-                  }
-                </GridList>
-              </Grid>
-            </Grid>
+        {uploadDialog}
 
-          </DialogContent>
-          <DialogActions>
-            <Button variant="outlined" color="primary" onClick={handlerUpload}>アップロード</Button>
-            <Button variant="outlined" color="primary" onClick={handleDialogClose}>キャンセル</Button>
-          </DialogActions>
+        {versionDialog}
 
-
-        </Dialog>
       </div>
     </GenericTemplate>
   )
