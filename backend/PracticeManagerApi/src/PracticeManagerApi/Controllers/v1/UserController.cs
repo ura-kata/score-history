1a9a6d37382bcab7ed2128dc1b04352a76807530
uttne <uttne.mkr@gmail.com>
2021-01-14 01:33:46 +0900

    ユーザー情報取得用のAPI追加

---
+++ backend/PracticeManagerApi/src/PracticeManagerApi/Controllers/v1/UserController.cs
@@ -0,0 +1,57 @@
+using System.Dynamic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace PracticeManagerApi.Controllers.v1
+{
+    public class UserMe
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Id { get; set; }
+    }
+
+    [Route("api/v1/user")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(IConfiguration configuration, ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("me")]
+        [HttpGet]
+        public UserMe GetMe()
+        {
+            string id = null;
+            string name = null;
+            string email = null;
+
+            foreach (var claim in Request.HttpContext.User.Claims)
+            {
+                switch (claim.Type.ToLowerInvariant())
+                {
+                    case "principalid":
+                        id = claim.Value;
+                        break;
+                    case "email":
+                        email = claim.Value;
+                        break;
+                    case "cognito:username":
+                        name = claim.Value;
+                        break;
+                }
+            }
+
+            return new UserMe()
+            {
+                Name = name,
+                Id = id,
+                Email = email
+            };
+        }
+    }
+}
