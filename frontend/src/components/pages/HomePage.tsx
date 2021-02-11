98ab27ad5256aa39a9e567f1cf2c89ce6509f083
uttne <uttne.mkr@gmail.com>
2021-02-11 19:51:25 +0900

    format

--- frontend/src/components/pages/HomePage.tsx
+++ frontend/src/components/pages/HomePage.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useMemo, useState } from 'react';
-import GenericTemplate from '../templates/GenericTemplate'
+import React, { useEffect, useMemo, useState } from "react";
+import GenericTemplate from "../templates/GenericTemplate";
 import {
   createStyles,
   makeStyles,
@@ -15,18 +15,30 @@ import {
   Button,
   Breadcrumbs,
   Paper,
-} from '@material-ui/core'
-import PracticeManagerApiClient, { Score, ScoreVersion, ScoreVersionPage } from '../../PracticeManagerApiClient'
+} from "@material-ui/core";
+import PracticeManagerApiClient, {
+  Score,
+  ScoreVersion,
+  ScoreVersionPage,
+} from "../../PracticeManagerApiClient";
 import { Link, useHistory, useRouteMatch } from "react-router-dom";
-import { Timeline, TimelineConnector, TimelineContent, TimelineDot, TimelineItem, TimelineSeparator } from '@material-ui/lab';
-import ScorePageDetailDialog from '../organisms/ScorePageDetailDialog';
+import {
+  Timeline,
+  TimelineConnector,
+  TimelineContent,
+  TimelineDot,
+  TimelineItem,
+  TimelineSeparator,
+} from "@material-ui/lab";
+import ScorePageDetailDialog from "../organisms/ScorePageDetailDialog";
 
-const client = new PracticeManagerApiClient(process.env.REACT_APP_API_URI_BASE as string);
-
+const client = new PracticeManagerApiClient(
+  process.env.REACT_APP_API_URI_BASE as string
+);
 
 // ------------------------------------------------------------------------------------------
-interface ScoreListViewProps{
-  scores: {[name: string]: Score};
+interface ScoreListViewProps {
+  scores: { [name: string]: Score };
   onClick?: (key: string, score: Score) => void;
 }
 
@@ -36,75 +48,88 @@ const ScoreListView = (props: ScoreListViewProps) => {
 
   const classes = makeStyles((theme: Theme) =>
     createStyles({
-      scoreCard:{
+      scoreCard: {
         width: "300px",
-        margin: theme.spacing(1)
+        margin: theme.spacing(1),
       },
-      scoreCardName:{
-        color: colors.grey[400]
+      scoreCardName: {
+        color: colors.grey[400],
       },
-      scoreCardContainer:{
-        margin: theme.spacing(3,0,0)
-      }
-    }))();
+      scoreCardContainer: {
+        margin: theme.spacing(3, 0, 0),
+      },
+    })
+  )();
 
   return (
     <Grid container className={classes.scoreCardContainer}>
-        {
-           Object.entries(_scores).map(([key, score], i)=>(
-            <Card key={i.toString()} className={classes.scoreCard}>
-              <CardActionArea onClick={()=>{if(_onClick) _onClick(key, score);}}>
-                <CardContent>
-                  <Typography variant="h5">{score.title}</Typography>
-                  <Typography variant="caption" className={classes.scoreCardName}>{score.name}</Typography>
-                  <Typography variant="subtitle1" gutterBottom>{score.description}</Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          ))
-        }
-      </Grid>
+      {Object.entries(_scores).map(([key, score], i) => (
+        <Card key={i.toString()} className={classes.scoreCard}>
+          <CardActionArea
+            onClick={() => {
+              if (_onClick) _onClick(key, score);
+            }}
+          >
+            <CardContent>
+              <Typography variant="h5">{score.title}</Typography>
+              <Typography variant="caption" className={classes.scoreCardName}>
+                {score.name}
+              </Typography>
+              <Typography variant="subtitle1" gutterBottom>
+                {score.description}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      ))}
+    </Grid>
   );
 };
 
 // ------------------------------------------------------------------------------------------
 
-interface ScoreDetailContentProps{
+interface ScoreDetailContentProps {
   score?: Score;
 }
 
 const ScoreDetailContent = (props: ScoreDetailContentProps) => {
   const _socre = props.score;
 
-  useEffect(()=>{
-    const f = async ()=>{
-      if(!_socre) return;
+  useEffect(() => {
+    const f = async () => {
+      if (!_socre) return;
     };
     f();
-  },[_socre]);
+  }, [_socre]);
 
-  const timelineItems = !_socre ? [] : [..._socre.versions].reverse().map((version, index)=>{
-
-    return (
-      <TimelineItem key={index}>
-        <TimelineSeparator>
-          <TimelineDot>
-            {/* Todo チェックしたかどうかをアイコンで表示する */}
-          </TimelineDot>
-          {index !== (_socre.versions.length - 1) ? (<TimelineConnector />) : (<></>)}
-        </TimelineSeparator>
-        <TimelineContent>
-          <Button component={Link} to={`/home/${_socre?.name}/${version.version}`}>
-            <Paper elevation={3} style={{padding: "6px 16px"}}>
-              <Typography>version {version.version}</Typography>
-            </Paper>
-
-          </Button>
-
-        </TimelineContent>
-      </TimelineItem>
-    );
-  });
+  const timelineItems = !_socre
+    ? []
+    : [..._socre.versions].reverse().map((version, index) => {
+        return (
+          <TimelineItem key={index}>
+            <TimelineSeparator>
+              <TimelineDot>
+                {/* Todo チェックしたかどうかをアイコンで表示する */}
+              </TimelineDot>
+              {index !== _socre.versions.length - 1 ? (
+                <TimelineConnector />
+              ) : (
+                <></>
+              )}
+            </TimelineSeparator>
+            <TimelineContent>
+              <Button
+                component={Link}
+                to={`/home/${_socre?.name}/${version.version}`}
+              >
+                <Paper elevation={3} style={{ padding: "6px 16px" }}>
+                  <Typography>version {version.version}</Typography>
+                </Paper>
+              </Button>
+            </TimelineContent>
+          </TimelineItem>
+        );
+      });
 
   return (
     <>
@@ -114,7 +139,7 @@ const ScoreDetailContent = (props: ScoreDetailContentProps) => {
         </Grid>
       </Grid>
 
-      <Divider/>
+      <Divider />
 
       <Grid container spacing={3}>
         <Grid item xs>
@@ -123,32 +148,33 @@ const ScoreDetailContent = (props: ScoreDetailContentProps) => {
               <Typography variant="h5">説明</Typography>
             </Grid>
             <Grid item xs={12}>
-              {_socre?.description?.split('\n').map((t,index)=>(<Typography key={index}>{t}</Typography>))}
+              {_socre?.description?.split("\n").map((t, index) => (
+                <Typography key={index}>{t}</Typography>
+              ))}
             </Grid>
           </Grid>
         </Grid>
         <Grid item xs={5}>
           <Grid container justify="center" spacing={3}>
             <Grid item xs={12}>
-              <Typography variant="h5" style={{textAlign: "center"}}>バージョン</Typography>
+              <Typography variant="h5" style={{ textAlign: "center" }}>
+                バージョン
+              </Typography>
             </Grid>
             <Grid item xs={12}>
               {/* Todo バージョンは長くなることが良そうされるのでスクロールできるようにする */}
-              <Timeline align="left">
-                {timelineItems}
-              </Timeline>
+              <Timeline align="left">{timelineItems}</Timeline>
             </Grid>
           </Grid>
         </Grid>
       </Grid>
     </>
   );
-}
-
+};
 
 // ------------------------------------------------------------------------------------------
 
-interface ScoreVersionDetailContentProps{
+interface ScoreVersionDetailContentProps {
   score?: Score;
   version?: number;
   pageNo?: number;
@@ -159,69 +185,95 @@ const ScoreVersionDetailContent = (props: ScoreVersionDetailContentProps) => {
   const _version = props.version;
   const _pageNo = props.pageNo;
 
-  const [scoreVersion, setScoreVersion] = useState<ScoreVersion | undefined>(undefined);
+  const [scoreVersion, setScoreVersion] = useState<ScoreVersion | undefined>(
+    undefined
+  );
   const history = useHistory();
 
-  useEffect(()=>{
-    if(!_socre) return;
-    if(_version === undefined) return;
+  useEffect(() => {
+    if (!_socre) return;
+    if (_version === undefined) return;
 
-    const f = async ()=>{
-      try{
+    const f = async () => {
+      try {
         const sv = await client.getScoreVersion(_socre.name, _version);
         setScoreVersion(sv);
-      }
-      catch(err){
+      } catch (err) {
         console.log(err);
       }
     };
     f();
-  },[_socre, _version]);
+  }, [_socre, _version]);
 
-  const actionsAndPageSet = useMemo(()=>{
+  const actionsAndPageSet = useMemo(() => {
+    const ret = {} as {
+      [no: number]: {
+        page: ScoreVersionPage;
+        onPrev?: () => void;
+        onNext?: () => void;
+      };
+    };
 
-    const ret = {} as {[no: number]: {page: ScoreVersionPage, onPrev?: ()=>void, onNext?: ()=>void}};
+    scoreVersion?.pages.forEach((page, index) => {
+      const prevUrl =
+        index === 0 || _socre === undefined || _version === undefined
+          ? undefined
+          : `/home/${_socre.name}/${_version}/${
+              scoreVersion.pages[index - 1].no
+            }/`;
 
-    scoreVersion?.pages.forEach((page, index)=>{
-      const prevUrl = (index === 0 || _socre === undefined || _version === undefined) ? undefined :
-        `/home/${_socre.name}/${_version}/${scoreVersion.pages[index - 1].no}/`;
-
-      const nextUrl = (index === (scoreVersion.pages.length - 1) || _socre === undefined || _version === undefined) ? undefined :
-      `/home/${_socre.name}/${_version}/${scoreVersion.pages[index + 1].no}/`;
+      const nextUrl =
+        index === scoreVersion.pages.length - 1 ||
+        _socre === undefined ||
+        _version === undefined
+          ? undefined
+          : `/home/${_socre.name}/${_version}/${
+              scoreVersion.pages[index + 1].no
+            }/`;
       ret[page.no] = {
         page: page,
-        onPrev: prevUrl ? ()=>history.push(prevUrl) : undefined,
-        onNext: nextUrl ? ()=>history.push(nextUrl) : undefined,
-      }
+        onPrev: prevUrl ? () => history.push(prevUrl) : undefined,
+        onNext: nextUrl ? () => history.push(nextUrl) : undefined,
+      };
     });
 
     return ret;
+  }, [_socre, _version, history, scoreVersion]);
 
-  },[_socre, _version, history, scoreVersion]);
+  const actionsAndPage =
+    _pageNo === undefined ? undefined : actionsAndPageSet[_pageNo];
 
-  const actionsAndPage = _pageNo === undefined ? undefined : actionsAndPageSet[_pageNo];
+  const thumbnailContents = !scoreVersion
+    ? []
+    : scoreVersion.pages.map((page, index) => {
+        return (
+          <Grid item key={page.no}>
+            <Button
+              component={Link}
+              to={`/home/${_socre?.name}/${_version}/${page.no}`}
+            >
+              <Paper>
+                <Grid container justify="center">
+                  <Grid item xs={12} style={{ textAlign: "center" }}>
+                    <img
+                      src={page.thumbnail_url ?? page.image_url}
+                      height={"200px"}
+                      alt={page.no.toString()}
+                      style={{ userSelect: "none" }}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Typography align="center">p. {page.no}</Typography>
+                  </Grid>
+                </Grid>
+              </Paper>
+            </Button>
+          </Grid>
+        );
+      });
 
-  const thumbnailContents = !scoreVersion ? [] : scoreVersion.pages.map((page, index)=>{
-    return (
-      <Grid item key={page.no}>
-        <Button component={Link} to={`/home/${_socre?.name}/${_version}/${page.no}`}>
-          <Paper>
-            <Grid container justify="center">
-              <Grid item xs={12} style={{textAlign: "center"}}>
-                <img src={page.thumbnail_url ?? page.image_url} height={"200px"} alt={page.no.toString()} style={{userSelect: "none"}}/>
-              </Grid>
-              <Grid item xs={12}>
-                <Typography align="center">p. {page.no}</Typography>
-              </Grid>
-            </Grid>
-          </Paper>
-        </Button>
-      </Grid>
-    );
-  });
-
-  const handleOnClose = ()=>{
-    if(!_socre) return;
+  const handleOnClose = () => {
+    if (!_socre) return;
     history.push(`/home/${_socre.name}/${_version}/`);
   };
 
@@ -233,27 +285,33 @@ const ScoreVersionDetailContent = (props: ScoreVersionDetailContentProps) => {
         </Grid>
       </Grid>
 
-      <Divider/>
+      <Divider />
 
       <Grid container spacing={3}>
         <Grid item xs={12}>
           <Typography variant="h5">バージョン {_version}</Typography>
         </Grid>
         <Grid item xs={4}>
-          <Grid container spacing={3}>
-
-          </Grid>
+          <Grid container spacing={3}></Grid>
           <Grid container spacing={3}>
             <Grid item xs={12}>
               <Typography variant="h5">説明</Typography>
             </Grid>
             <Grid item xs={12}>
-              {scoreVersion?.description?.split('\n').map((t,index)=>(<Typography key={index}>{t}</Typography>))}
+              {scoreVersion?.description?.split("\n").map((t, index) => (
+                <Typography key={index}>{t}</Typography>
+              ))}
             </Grid>
           </Grid>
         </Grid>
         <Grid item xs={8}>
-          <Grid container alignItems="flex-start" justify="flex-start" alignContent="flex-start" spacing={1}>
+          <Grid
+            container
+            alignItems="flex-start"
+            justify="flex-start"
+            alignContent="flex-start"
+            spacing={1}
+          >
             {thumbnailContents}
           </Grid>
         </Grid>
@@ -268,25 +326,25 @@ const ScoreVersionDetailContent = (props: ScoreVersionDetailContentProps) => {
       />
     </>
   );
-}
+};
 
 // ------------------------------------------------------------------------------------------
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    scoreCard:{
+    scoreCard: {
       width: "300px",
-      margin: theme.spacing(1)
+      margin: theme.spacing(1),
     },
-    scoreCardName:{
-      color: colors.grey[400]
+    scoreCardName: {
+      color: colors.grey[400],
     },
-    scoreCardContainer:{
-      margin: theme.spacing(3,0,0)
+    scoreCardContainer: {
+      margin: theme.spacing(3, 0, 0),
     },
-    breadcrumbsLink:{
-      textDecoration: "none"
-    }
+    breadcrumbsLink: {
+      textDecoration: "none",
+    },
   })
 );
 
@@ -295,61 +353,59 @@ type HomeContentType = "home" | "detail" | "version" | "page";
 const HomePage = () => {
   const classes = useStyles();
 
-  const [scores, setScores] = useState<{[name: string]:Score}>({});
+  const [scores, setScores] = useState<{ [name: string]: Score }>({});
   const history = useHistory();
   const urlMatch = useRouteMatch<{
-    scoreName?: string,
-    version?: string,
-    pageNo?: string}>("/home/:scoreName?/:version?/:pageNo?");
+    scoreName?: string;
+    version?: string;
+    pageNo?: string;
+  }>("/home/:scoreName?/:version?/:pageNo?");
 
-  useEffect(()=>{
-    const f = async ()=>{
-
-      try{
+  useEffect(() => {
+    const f = async () => {
+      try {
         const response = await client.getScores();
 
-        const s: {[name: string]: Score} = {};
-        response.forEach(x=>s[x.name] = x);
+        const s: { [name: string]: Score } = {};
+        response.forEach((x) => (s[x.name] = x));
         setScores(s);
-      }
-      catch(err){
+      } catch (err) {
         console.log(err);
         setScores({});
       }
     };
 
     f();
-
-  },[]);
+  }, []);
 
   let contentType: HomeContentType = "home";
 
   let score: undefined | Score = undefined;
-  if(urlMatch){
+  if (urlMatch) {
     const scoreName = urlMatch.params.scoreName;
-    if(scoreName){
+    if (scoreName) {
       score = scores[scoreName];
 
-      if(score){
+      if (score) {
         contentType = "detail";
       }
     }
   }
 
   let version: undefined | number = undefined;
-  if(urlMatch){
+  if (urlMatch) {
     const versinText = urlMatch.params.version;
     version = versinText !== undefined ? parseInt(versinText) : undefined;
-    if(version !== undefined){
+    if (version !== undefined) {
       contentType = "version";
     }
   }
 
   let pageNo: undefined | number = undefined;
-  if(urlMatch){
+  if (urlMatch) {
     const pageNoText = urlMatch.params.pageNo;
     pageNo = pageNoText !== undefined ? parseInt(pageNoText) : undefined;
-    if(pageNo){
+    if (pageNo) {
       contentType = "page";
     }
   }
@@ -357,8 +413,8 @@ const HomePage = () => {
   const handleScoreOnClick = (key: string, socre: Score) => {
     history.push(`/home/${key}/`);
   };
-  const content = ((type: HomeContentType)=>{
-    switch(type){
+  const content = ((type: HomeContentType) => {
+    switch (type) {
       case "home": {
         return (
           <>
@@ -367,34 +423,27 @@ const HomePage = () => {
                 <Typography variant="h4">スコア一覧</Typography>
               </Grid>
               <Grid item xs>
-                <ButtonGroup color="primary" style={{float: "right"}}>
-                  <Button component={Link} to="/new">新規</Button>
+                <ButtonGroup color="primary" style={{ float: "right" }}>
+                  <Button component={Link} to="/new">
+                    新規
+                  </Button>
                 </ButtonGroup>
               </Grid>
             </Grid>
 
-            <Divider/>
+            <Divider />
 
-            <ScoreListView scores={scores} onClick={handleScoreOnClick}/>
+            <ScoreListView scores={scores} onClick={handleScoreOnClick} />
           </>
         );
       }
       case "detail": {
-        return (
-          <ScoreDetailContent
-            score={score}
-          />
-        );
+        return <ScoreDetailContent score={score} />;
       }
-      case "version":{
-        return (
-          <ScoreVersionDetailContent
-            score={score}
-            version={version}
-          />
-        );
+      case "version": {
+        return <ScoreVersionDetailContent score={score} version={version} />;
       }
-      case "page":{
+      case "page": {
         return (
           <ScoreVersionDetailContent
             score={score}
@@ -403,29 +452,47 @@ const HomePage = () => {
           />
         );
       }
-      default: return (<></>)
-  }})(contentType);
+      default:
+        return <></>;
+    }
+  })(contentType);
 
-  const breadcrumbList = [(<Button key={0} component={Link} to="/">Home</Button>)];
-  if(score){
-    breadcrumbList.push((<Button key={breadcrumbList.length} component={Link} to={`/home/${score.name}/`}>{score.name}</Button>));
+  const breadcrumbList = [
+    <Button key={0} component={Link} to="/">
+      Home
+    </Button>,
+  ];
+  if (score) {
+    breadcrumbList.push(
+      <Button
+        key={breadcrumbList.length}
+        component={Link}
+        to={`/home/${score.name}/`}
+      >
+        {score.name}
+      </Button>
+    );
 
-    if(version){
-      breadcrumbList.push((<Button key={breadcrumbList.length} component={Link} to={`/home/${score.name}/${version}`}>version {version}</Button>));
+    if (version) {
+      breadcrumbList.push(
+        <Button
+          key={breadcrumbList.length}
+          component={Link}
+          to={`/home/${score.name}/${version}`}
+        >
+          version {version}
+        </Button>
+      );
     }
   }
 
   return (
     <GenericTemplate>
-      <Breadcrumbs>
-        {breadcrumbList}
-      </Breadcrumbs>
+      <Breadcrumbs>{breadcrumbList}</Breadcrumbs>
 
       {content}
-
     </GenericTemplate>
   );
-
-}
+};
 
 export default HomePage;
