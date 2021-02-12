61c316fb6f55e2297e498c18209ace4a2e4c4714
uttne <uttne.mkr@gmail.com>
2021-02-13 02:51:14 +0900

    楽譜を更新するためのコンポーネントのファイルを追加

---
+++ frontend/src/components/organisms/UpdateScoreContent.tsx
@@ -0,0 +1,14 @@
+import { Grid } from "@material-ui/core";
+import React from "react";
+
+export interface UpdateScoreContentProps {}
+
+const UpdateScoreContent = (props: UpdateScoreContentProps) => {
+  return (
+    <Grid container>
+      <Grid item></Grid>
+    </Grid>
+  );
+};
+
+export default UpdateScoreContent;
