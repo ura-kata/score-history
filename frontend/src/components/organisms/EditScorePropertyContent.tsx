9524a1d7dcaf909c8c85040bc6d90783b559e1d1
uttne <uttne.mkr@gmail.com>
2021-02-14 03:37:46 +0900

    UpdateScore を EditScoreProperty に変更して追加

---
+++ frontend/src/components/organisms/EditScorePropertyContent.tsx
@@ -0,0 +1,111 @@
+import {
+  Button,
+  createStyles,
+  Grid,
+  makeStyles,
+  TextField,
+  Theme,
+  Typography,
+} from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
+import React, { useState } from "react";
+import { scoreClient } from "../../global";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    textField: {
+      width: "100%",
+    },
+  })
+);
+
+export interface EditScorePropertyContentProps {
+  owner: string;
+  scoreName: string;
+  title?: string;
+  description?: string;
+}
+
+const EditScorePropertyContent = (props: EditScorePropertyContentProps) => {
+  const classes = useStyles();
+
+  const _owner = props.owner;
+  const _scoreName = props.scoreName;
+
+  const _title = props.title;
+  const _description = props.description;
+  const [title, setTitle] = useState(_title);
+  const [description, setDescription] = useState(_description);
+  const [errorMessage, setErrorMessage] = useState<string>();
+
+  const handleOnChangeTitle = (event: any) => {
+    setTitle(event.target.value);
+  };
+  const handleOnChangeDescription = (event: any) => {
+    setDescription(event.target.value);
+  };
+
+  const handleOnClickUpdate = async () => {
+    const oldProperty = {
+      title: props.title,
+      description: props.description,
+    };
+    const newProperty = {
+      title: title,
+      description: description,
+    };
+    try {
+      await scoreClient.updateProperty(
+        _owner,
+        _scoreName,
+        oldProperty,
+        newProperty
+      );
+      setErrorMessage(undefined);
+    } catch (err) {
+      console.log(err);
+      setErrorMessage("変更に失敗しました");
+    }
+  };
+
+  const disableUpdateButton =
+    (_title ?? "") === (title ?? "") &&
+    (_description ?? "") === (description ?? "");
+
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <TextField
+          label="楽譜のタイトル"
+          className={classes.textField}
+          value={title}
+          onChange={handleOnChangeTitle}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          label="楽譜の説明"
+          multiline
+          className={classes.textField}
+          value={description}
+          onChange={handleOnChangeDescription}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        {errorMessage ? <Alert severity="error">{errorMessage}</Alert> : <></>}
+      </Grid>
+      <Grid item xs={12}>
+        <Button
+          variant="outlined"
+          color="primary"
+          onClick={handleOnClickUpdate}
+          disabled={disableUpdateButton}
+        >
+          更新
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default EditScorePropertyContent;
