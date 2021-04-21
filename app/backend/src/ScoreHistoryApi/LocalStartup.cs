57685db7e1e786e01e0ea7d23703ebe536dfe20a
uttne <uttne.mkr@gmail.com>
2021-04-22 00:59:23 +0900

    add swagger

--- app/backend/src/ScoreHistoryApi/LocalStartup.cs
+++ app/backend/src/ScoreHistoryApi/LocalStartup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace ScoreHistoryApi
 {
@@ -26,6 +20,14 @@ namespace ScoreHistoryApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(option =>
+            {
+                option.SwaggerDoc("dev",new OpenApiInfo()
+                {
+                    Version = "dev"
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +47,12 @@ namespace ScoreHistoryApi
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/dev/swagger.json", "Ura-Kata Score History API");
             });
         }
     }
