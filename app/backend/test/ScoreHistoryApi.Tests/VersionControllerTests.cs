2a09f9b914b340ff3947307f97b483dc846c4b1f
uttne <uttne.mkr@gmail.com>
2021-04-21 23:48:58 +0900

    API のテストプロジェクトを追加

---
+++ app/backend/test/ScoreHistoryApi.Tests/VersionControllerTests.cs
@@ -0,0 +1,36 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.TestUtilities;
+using Newtonsoft.Json;
+using ScoreHistoryApi.Models.Version;
+using Xunit;
+
+namespace ScoreHistoryApi.Tests
+{
+    public class VersionControllerTests
+    {
+        [Fact]
+        public async Task TestSuccessGetVersion()
+        {
+            var lambdaFunction = new LambdaEntryPoint();
+            var requestText = await File.ReadAllTextAsync("./VersionRequests/VersionController-Get.json");
+            var request = JsonConvert.DeserializeObject<APIGatewayProxyRequest>(requestText);
+            var context = new TestLambdaContext();
+
+            var version = "dev";
+
+            Startup.Configuration[EnvironmentNames.ApiVersion] = version;
+
+            var response = await lambdaFunction.FunctionHandlerAsync(request, context);
+
+            Assert.Equal(200, response.StatusCode);
+
+            var json = JsonConvert.DeserializeObject<ApiVersion>(response.Body);
+
+
+            Assert.Equal(version, json.Version);
+        }
+    }
+}
