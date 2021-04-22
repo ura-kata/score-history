7bbdd3705439303de0367dc5c845b68b06afaa51
uttne <uttne.mkr@gmail.com>
2021-04-23 01:33:37 +0900

    Claim から認証情報を取得する拡張メソッドを追加

---
+++ app/backend/src/ScoreHistoryApi/Models/AuthorizerData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ScoreHistoryApi.Models
+{
+    public class AuthorizerData
+    {
+        public Guid Principalid { get; set; }
+        public Guid Sub { get; set; }
+        public string Email { get; set; }
+        public string CognitoUserName { get; set; }
+    }
+}
