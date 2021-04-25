3da2e71638ba0596704d88a45f6a8bd678724a2b
uttne <uttne.mkr@gmail.com>
2021-04-26 01:48:53 +0900

    API にコメントを追加

--- app/backend/src/ScoreHistoryApi/Controllers/ScoresController.cs
+++ app/backend/src/ScoreHistoryApi/Controllers/ScoresController.cs
@@ -26,6 +26,11 @@ namespace ScoreHistoryApi.Controllers
 
         #region user
 
+        /// <summary>
+        /// ログインユーザーがアクセス可能な楽譜の一覧を取得する
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
         [HttpGet]
         [Route("user")]
         public async Task<ActionResult<ScoreSummary[]>> GetUserScoresAsync()
@@ -40,6 +45,12 @@ namespace ScoreHistoryApi.Controllers
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// ログインユーザーの楽譜を作成する
+        /// </summary>
+        /// <param name="newScore"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
         [HttpPost]
         [Route("user")]
         public async Task<ActionResult<ScoreDetail>> CreateUserScoreAsync([FromBody] NewScore newScore)
@@ -47,13 +58,25 @@ namespace ScoreHistoryApi.Controllers
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// ログインユーザーの単一の楽譜の詳細情報を取得する
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
         [HttpGet]
         [Route("user/{id:guid}")]
         public Task<ActionResult<ScoreDetail>> GetAUserScoreAsync([FromRoute(Name = "id")] Guid id)
         {
             throw new NotImplementedException();
         }
-        
+
+        /// <summary>
+        /// ログインユーザーの指定された楽譜を削除する
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
         [HttpDelete]
         [Route("user/{id:guid}")]
         public Task<IActionResult> DeleteAUserScoreAsync([FromRoute(Name = "id")] Guid id)
@@ -61,6 +84,12 @@ namespace ScoreHistoryApi.Controllers
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// ログインユーザーの指定された楽譜を更新する
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
         [HttpPatch]
         [Route("user/{id:guid}")]
         public Task<IActionResult> PatchAUserScoreAsync([FromRoute(Name = "id")] Guid id)
@@ -77,6 +106,12 @@ namespace ScoreHistoryApi.Controllers
 
         #region owner
 
+        /// <summary>
+        /// 指定された owner の所有している楽譜一覧を取得する
+        /// </summary>
+        /// <param name="owner"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
         [HttpGet]
         [Route("{owner:guid}")]
         public Task<ActionResult<ScoreSummary[]>> GetOwnerScoresAsync([FromRoute(Name = "owner")] Guid owner)
@@ -84,6 +119,13 @@ namespace ScoreHistoryApi.Controllers
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// 指定された owner の所有している単一の楽譜の詳細情報を取得する
+        /// </summary>
+        /// <param name="owner"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
         [HttpGet]
         [Route("{owner:guid}/{id:guid}")]
         public Task<ActionResult<ScoreDetail>> GetAOwnerScoreAsync(
