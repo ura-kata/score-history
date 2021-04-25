94c2689d8824cf4a4b71020a9806d0e9b25be4d7
uttne <uttne.mkr@gmail.com>
2021-04-26 01:52:17 +0900

    オブジェクトストレージインターフェースのメソッドを追加

--- app/backend/src/ScoreHistoryApi.AWSWrapper/Storage/IObjectStorage.cs
+++ app/backend/src/ScoreHistoryApi.AWSWrapper/Storage/IObjectStorage.cs
@@ -1,10 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace ScoreHistoryApi.AWSWrapper.Storage
 {
     public interface IObjectStorage
     {
+        /// <summary>
+        /// オブジェクトを保存する
+        /// </summary>
+        /// <param name="path">object のパス</param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        void SaveObject(string path, byte[] data);
+
+        /// <summary>
+        /// 指定したパスのオブジェクトデータを削除する
+        /// </summary>
+        /// <param name="path">object のパス</param>
+        void DeleteObject(string path);
     }
 }
