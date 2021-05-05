c159b8ec9b1876649c9495b72366a8a1ea995f10
uttne <uttne.mkr@gmail.com>
2021-05-06 00:31:42 +0900

    fix SavedItemData で使用するクラスを追加

---
+++ app/backend/src/ScoreHistoryApi/Logics/ScoreObjectStorages/Thumbnail.cs
@@ -0,0 +1,11 @@
+namespace ScoreHistoryApi.Logics.ScoreObjectStorages
+{
+    /// <summary>
+    /// サムネイル情報
+    /// </summary>
+    public class Thumbnail
+    {
+        /// <summary> S3 のオブジェクト名 </summary>
+        public string ObjectName { get; set; }
+    }
+}
