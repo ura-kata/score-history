71cbb8ec65c96ea0cc1b803b5853d5d25c5328e1
uttne <uttne.mkr@gmail.com>
2021-05-29 20:09:52 +0900

    Sign Out した後に SignIn Page に遷移する

--- app/frontend/src/components/organisms/MainAppBar.tsx
+++ app/frontend/src/components/organisms/MainAppBar.tsx
@@ -69,6 +69,7 @@ export default function MainAppBar(props: MainAppBarProps) {
   const handleSignOut = async () => {
     try {
       await accessClient.signout();
+      accessClient.gotoSignInPage("");
     } catch (err) {
       console.log(err);
     }
