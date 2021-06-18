c0d4c547dba279df79228ca82c72f83c5c371710
uttne <uttne.mkr@gmail.com>
2021-06-18 22:03:16 +0900

    楽譜の編集可能な最新データの表示部分をコンポーネントに分離

--- app/frontend/src/components/organisms/ScoreDetailContent.tsx
+++ app/frontend/src/components/organisms/ScoreDetailContent.tsx
@@ -12,10 +12,10 @@ import { AppContext } from "../../AppContext";
 import useMeyScoreDetail from "../../hooks/scores/useMeyScoreDetail";
 import DetailEditableDescription from "../atoms/DetailEditableDescription";
 import DetailEditableTitle from "../atoms/DetailEditableTitle";
+import LatestScoreContent from "../atoms/LatestScoreContent";
 import PageContent from "../atoms/PageContent";
 import SnapshotNameList from "../atoms/SnapshotNameList";
 import { ThumbnailListContent } from "../atoms/ThumbnailListContent";
-import EditIcon from "@material-ui/icons/Edit";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -33,36 +33,11 @@ const useStyles = makeStyles((theme: Theme) =>
       width: "100%",
       display: "flex",
     },
-    infoContainer: {
-      width: "100%",
-    },
-    titleContainer: {
-      width: "100%",
-    },
-    descContainer: {
-      width: "100%",
-    },
-    descP: {},
-    thumbnailRoot: {
-      width: "100%",
-      display: "flex",
-      flexFlow: "column",
-    },
-    thumbnailControlBar: {
-      width: "100%",
-      height: "50px",
-      display: "flex",
-      justifyContent: "flex-end",
-    },
-    thumbnailContainer: {
-      width: "100%",
-      display: "flex",
+    scoreDataArea: {
+      width: "calc(100% - 200px)",
     },
     snapshotArea: {
       width: "200px",
-    },
-    scoreArea: {
-      width: "calc(100% - 200px)",
     },
   })
 );
@@ -80,50 +55,13 @@ export default function ScoreDetailContent(props: ScoreDetailContentProps) {
   const classes = useStyles();
 
   const { scoreId, pageId, snapshotId } = useParams<PathParameters>();
+
   const history = useHistory();
-
-  const [detail, updateDetail] = useMeyScoreDetail({ scoreId, retryCount: 3 });
-
-  const [title, setTitle] = useState<string>("");
-  const [description, setDescription] = useState<string>("");
-
-  const appContext = React.useContext(AppContext);
-
-  const _userData = appContext.userData;
-
-  useEffect(() => {
-    setTitle(detail?.data.title ?? "");
-
-    const desc = detail?.hashSet
-      ? detail.hashSet[detail.data.descriptionHash]
-      : "";
-    setDescription(desc);
-  }, [detail]);
-
-  const pages = useMemo(() => {
-    return [...(detail?.data.pages ?? [])].sort((x, y) => {
-      const xn = parseInt("0" + x.page);
-      const yn = parseInt("0" + y.page);
-      if (xn < yn) return -1;
-      if (yn < xn) return 1;
-      return 0;
-    });
-  }, [detail]);
 
   const handleBack = () => {
     history.push("/");
   };
 
-  const handleOnChangeTitle = (newTitle: string) => {
-    setTitle(newTitle);
-  };
-
-  const handleOnChangeDescription = (newDescription: string) => {
-    setDescription(newDescription);
-  };
-  const handleOnPageEditClick = () => {
-    history.push(`/scores/${scoreId}/edit-page`);
-  };
   return (
     <div className={classes.root}>
       <div className={classes.controlBar}>
@@ -139,43 +77,8 @@ export default function ScoreDetailContent(props: ScoreDetailContentProps) {
         </Button>
       </div>
       <div className={classes.contentRoot}>
-        <div className={classes.scoreArea}>
-          <div className={classes.infoContainer}>
-            <div className={classes.titleContainer}>
-              <DetailEditableTitle
-                id={scoreId}
-                title={title}
-                onChangeTitle={handleOnChangeTitle}
-              />
-            </div>
-            <div className={classes.descContainer}>
-              <DetailEditableDescription
-                id={scoreId}
-                description={description}
-                onChangeDescription={handleOnChangeDescription}
-              />
-            </div>
-          </div>
-          <div className={classes.thumbnailRoot}>
-            <div className={classes.thumbnailControlBar}>
-              <IconButton onClick={handleOnPageEditClick}>
-                <EditIcon />
-              </IconButton>
-            </div>
-            <div className={classes.thumbnailContainer}>
-              {/* <ThumbnailListContent
-            ownerId={_userData?.id}
-            scoreId={scoreId}
-            pages={detail?.data.pages}
-          /> */}
-              <PageContent
-                ownerId={_userData?.id}
-                scoreId={scoreId}
-                pages={pages}
-                pageId={pageId}
-              />
-            </div>
-          </div>
+        <div className={classes.scoreDataArea}>
+          <LatestScoreContent />
         </div>
         <div className={classes.snapshotArea}>
           <SnapshotNameList scoreId={scoreId} />
