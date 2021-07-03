41cfa2b40e9bd4fe7dc4e3d18befa0c74ce61add
uttne <uttne.mkr@gmail.com>
2021-07-04 00:11:26 +0900

    DynamoDB にかかわる共通処理を行うクラスを追加

---
+++ app/backend/src/ScoreHistoryApi/Logics/ScoreCommonLogic.cs
@@ -0,0 +1,63 @@
+#nullable enable
+using System;
+
+namespace ScoreHistoryApi.Logics
+{
+    public interface IScoreCommonLogic
+    {
+        /// <summary>
+        /// Guid を DynamoDB に保存するために文字列に変換する
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        string ConvertIdFromGuid(Guid id);
+
+        /// <summary>
+        /// DynamoDB に保存した ID を Guid に変換する
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Guid ConvertIdFromDynamo(string id);
+
+        /// <summary>
+        /// 現在時間
+        /// </summary>
+        DateTimeOffset Now { get; }
+
+        /// <summary>
+        /// 新しい Guid を作成する
+        /// </summary>
+        Guid NewGuid();
+    }
+
+    /// <summary>
+    /// 共通ロジック
+    /// </summary>
+    public class ScoreCommonLogic : IScoreCommonLogic
+    {
+        /// <summary>
+        /// Guid を DynamoDB に保存するために文字列に変換する
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public string ConvertIdFromGuid(Guid id) => Convert.ToBase64String(id.ToByteArray()).Substring(0, 22);
+
+        /// <summary>
+        /// DynamoDB に保存した ID を Guid に変換する
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Guid ConvertIdFromDynamo(string id) => new Guid(Convert.FromBase64String(id + "=="));
+
+        /// <summary>
+        /// 現在時間
+        /// </summary>
+        public DateTimeOffset Now => DateTimeOffset.Now;
+
+        /// <summary>
+        /// 新しい Guid を作成する
+        /// </summary>
+        /// <returns></returns>
+        public Guid NewGuid() => Guid.NewGuid();
+    }
+}
